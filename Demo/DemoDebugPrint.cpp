ad1bf614a0f9bb2faca7ed1664a39aa5137be0e9
Joep <jsuijs@gmail.com>
2018-02-11 10:12:42 +0100

    Show use of debug based on streams.

--- Demo/DemoDebugPrint.cpp
+++ Demo/DemoDebugPrint.cpp
@@ -1,7 +1,7 @@
 //-----------------------------------------------------------------------------
 // DemoDebugPrint.cpp
 //
-// Copyright (c) 2013-2016 Joep Suijs - All rights reserved.        
+// Copyright (c) 2013-2018 Joep Suijs - All rights reserved.        
 //
 // This demo shows how to use various print functions for debugging.
 //
@@ -68,8 +68,8 @@ void DefaultDemoSetup()
    
    //--------------------------------------------------------------------------
    // Select the desired header format.
-   //   0 - (default) long header, contains level, module, fname, lineno
-   //   1 - short header, contains module and a letter to indicate level
+   //   0 - long header, contains level, module, fname, lineno
+   //   1 - (default) short header, contains module and a letter to indicate level
    //   2 - shortest header, Letter D plus module number.                                                               
    //   
    // See TDebug::MyPrintf in debug.cpp for more details (use the source, Luke)     
@@ -94,13 +94,13 @@ void DefaultDemoSetup()
    // 
    // Each level includes the lower levels. Levels can be changed runtime via 
    // the command line with 'dlevel <module> <level>'.    
-   Debug.SetLevel(RLM_RC, DBG_WARN);      // RC unhandled codes
-   Debug.SetLevel(101, DBG_INFO);         // User class
+   Debug.SetLevel(RLM_RC, DBG_WARN);         // RC unhandled codes
+   Debug.SetLevel(RLM_USER+1, DBG_INFO);     // User class 1 (0-based, maps currently to 101)
 
    // When TimeStampResolution is non-zero, timestamps are added to the log 
    // messages. Time stamps are 3 digits within parenthesis in units of
    // milliseconds / TimeStampResolution.
-   Debug.TimeStampResolution = 10;  // enable timestamps in 10ms resolution
+   Debug.TimeStampResolution = 10;           // enable timestamps in 10ms resolution
    
 }
 
@@ -110,19 +110,44 @@ void DefaultDemoSetup()
 //-----------------------------------------------------------------------------            
 void CliCmd_DefaultDemo(int NrParams, TCiParams *P)
 {  
-   printf("Demo of DebugPrint\n");
+   printf("Demo of DebugPrint - user class %d\n", RLM_USER+1);
    
-   INFO_printf(101,  "CliCmd_Demo called with %d params\n", NrParams);
-   WARN_printf(101,  "Be warned there is a debug message that won't be shown unless enter 'dlevel 101 3'\n");
-   DEBUG_printf(101, "This won't be visible with the default settings from DemoSetup\n");   
-   ERROR_printf(101, "This message can't be disabled by changing the debug level\n");
+   INFO_printf(RLM_USER+1,  "CliCmd_Demo called with %d params\n", NrParams);
+   WARN_printf(RLM_USER+1,  "Be warned there is a debug message that won't be shown unless enter 'dlevel 101 3'\n");
+   DEBUG_printf(RLM_USER+1, "This won't be visible with the default settings from DemoSetup\n");   
+   ERROR_printf(RLM_USER+1, "This message can't be disabled by changing the debug level\n");
 
    // Check level to execute code conditionally
-   if (Debug.IsLevel(101, DBG_WARN)) {
-      printf("Level 101 is Level is warning or higher (INFO, DEBUG).\n");
+   if (Debug.IsLevel(RLM_USER+1, DBG_WARN)) {
+      printf("Module %d level is warning or higher (INFO, DEBUG).\n", RLM_USER+1);
       printf("(Did you notice there is no header on these lines?)\n");
       const char *s = "TestString";
       HexDump(s, 8, 0);
-   }   
+   }    
+   
+   //--------------------------------------------------------------------------
+   // Stream based debug output                                     
+   //--------------------------------------------------------------------------
+   // Notes: 
+   //
+   // * The a-typical 'stream function call' determines if output is required 
+   //   and returns either 'cout' or 'nout' (no-out).   
+   //
+   // * Stream based debug only supports header type 1 and no timestamps.      
+   //
+   // * Use of printf vs streams is a heated topic 
+   //   (https://stackoverflow.com/questions/5328873/c-streams-vs-c-style-io) 
+   //
+   // * IMHO it is (at least) usefull for debug output of many variables, since
+   //   there the text and actual variable are located next to each other and
+   //   it is type-safe. (for example, see Um_MotorControl.cpp)                                
+   //
+   // * You can also use 'cout' for normal stream output.
+   //--------------------------------------------------------------------------
+   error(RLM_USER+1)    << "Stream output - error level on class   " << RLM_USER+1 << endl;
+   warning(RLM_USER+1)  << "Stream output - warning level on class " << RLM_USER+1 << endl;
+   info(RLM_USER+1)     << "Stream output - info level on class    " << RLM_USER+1 << endl;
+   debug(RLM_USER+1)    << "Stream output - debug level on class   " << RLM_USER+1 << endl;
+
 }                                
  
