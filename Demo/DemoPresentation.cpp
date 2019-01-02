b383425e9fb30586f98f7f5bc4336176a8314861
Joep <jsuijs@gmail.com>
2019-01-02 10:38:27 +0100

    DemoPresentation: added missing Publish-command in setup.

--- Demo/DemoPresentation.cpp
+++ Demo/DemoPresentation.cpp
@@ -1,36 +1,36 @@
 //-----------------------------------------------------------------------------
 // DemoPresentation.cpp
 //
-// Copyright (c) 2013-2018 Joep Suijs - All rights reserved.        
+// Copyright (c) 2013-2019 Joep Suijs - All rights reserved.
 //
 // This demo shows how to use the Presentation class.
 //
 // RobotLib tags: DEMO
 //-----------------------------------------------------------------------------
 // This file is part of RobotLib.
-// 
+//
 // RobotLib is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // RobotLib is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with RobotLib.  If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 // See http://wiki.robotmc.be/index.php?title=RobotLib for more information.
 //-----------------------------------------------------------------------------
 // This is a DEMO file.
 // A DEMO file can be included in a project by adding it to makefile.do.
-// For details, see: 
+// For details, see:
 //    http://wiki.robotmc.be/index.php?title=RobotLib_Modules#RobotLib_Demo.27s
 //-----------------------------------------------------------------------------
 // tags_end
-//----------------------------------------------------------------------------- 
+//-----------------------------------------------------------------------------
 #include "RobotLib.h"
 
 #define DEMO_NAME DemoPresentation
@@ -38,74 +38,77 @@
 //-------------
 // OVERVIEW
 //-------------
-/*     
+/*
    The presentation class provides information on the
    status of the robot for presentation. By default it
    provides the robot position (x, y and degrees).
-   Additional data can be added. 
+   Additional data can be added.
 
-   The format of the data is: 
+   The format of the data is:
    [DATA] P_x:4 P_y:0 Hd:0 [/DATA]
 
    The data is sent to the console port.
-   
+
    When the data is sent, is controlled by mode:
       0 - Off  (do not print)
       1 - Auto (print when the robot has moved)
-      2 - On   (print at fixed interval)        
+      2 - On   (print at fixed interval)
 */
 
 //-------------
-// DECLARATIONS 
+// DECLARATIONS
 //-------------
- 
+
 //-------------
-// INSTANCES 
+// INSTANCES
 //-------------
 static int Test0;
 static int Test1;
 
-//-----------------------------------------------------------------------------            
-// DefaultDemoSetup - 
-//-----------------------------------------------------------------------------            
-//-----------------------------------------------------------------------------            
+//-----------------------------------------------------------------------------
+// DefaultDemoSetup -
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 void DefaultDemoSetup()
-{       
-   printf("DemoSetup for Presentation.\n");  
-   
+{
+   printf("DemoSetup for Presentation.\n");
+
    // give test vars a distinctive value
    Test0 = 12345;
    Test1 = 98765;
-   
-   // Add tag 'dm' to provide Test0 data. 
-   Presentation.Add("dm", Test0); 
-   
+
+   // Add tag 'dm' to provide Test0 data.
+   Presentation.Add("dm", Test0);
+
    // Show only data when robot moves.
    Presentation.Mode = 1;
-   
-   // Show data once every 100ms                                   
+
+   // Show data once every 100ms
    Presentation.Interval.SetMs(100);
-                                         
+
    // Show configuration
-   Presentation.Dump();   
+   Presentation.Dump();
+
+   // Tell Publisher to publish data with message type 'PLANKTRICK'
+   Publisher.Add(RLM_PRESENTATION, "PLANKTRICK");
 }
 
-//----------------------------------------------------------------------------- 
-// CliCmd_DefaultDemo -           
-//-----------------------------------------------------------------------------            
-//-----------------------------------------------------------------------------            
+//-----------------------------------------------------------------------------
+// CliCmd_DefaultDemo -
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 void CliCmd_DefaultDemo(int NrParams, TCiParams *P)
-{  
+{
    printf("Demo command for Presentation.\n");
 
-   if (NrParams == 0) {  
+   if (NrParams == 0) {
       printf("Demo <n> (n=0..2) changes dm setup\n");
       Presentation.Dump();
-      return;   
+      return;
    }
 
-   switch(P[0].PInt) { 
-      case 0 : {              
+   switch(P[0].PInt) {
+      case 0 : {
          printf("Delete dm tag\n");
          Presentation.Delete("dm");
          break;
@@ -121,8 +124,8 @@ void CliCmd_DefaultDemo(int NrParams, TCiParams *P)
          break;
       }
    }
-}   
+}
 
 //-------------
-// OTHER CODE 
+// OTHER CODE
 //-------------
