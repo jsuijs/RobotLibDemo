d701f48660442f25a4ea9d2454ef7056264704bd
Joep <jsuijs@gmail.com>
2019-04-27 19:46:08 +0200

    Presentation class removed, update demo's  accordingly.

--- Demo/DemoSensorSimulator.cpp
+++ Demo/DemoSensorSimulator.cpp
@@ -1,36 +1,36 @@
 //-----------------------------------------------------------------------------
 // DemoSensorSimulator.cpp
 //
-// Copyright (c) 2013-2018 Joep Suijs - All rights reserved.        
+// Copyright (c) 2013-2019 Joep Suijs - All rights reserved.
 //
 // This demo shows how to use SensorSimulator.
 //
 // RobotLib tags: DEMO
 //-----------------------------------------------------------------------------
 // This file is part of RobotLib.
-// 
+//
 // RobotLib is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // RobotLib is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
-// along with RobotLib.  If not, see <http://www.gnu.org/licenses/>.
-// 
+// along with RobotLib. If not, see <http://www.gnu.org/licenses/>.
+//
 // See http://wiki.robotmc.be/index.php?title=RobotLib for more information.
 //-----------------------------------------------------------------------------
 // This is a DEMO file.
 // A DEMO file can be included in a project by adding it to makefile.do.
-// For details, see: 
+// For details, see:
 //    http://wiki.robotmc.be/index.php?title=RobotLib_Modules#RobotLib_Demo.27s
 //-----------------------------------------------------------------------------
 // tags_end
-//----------------------------------------------------------------------------- 
+//-----------------------------------------------------------------------------
 #include "RobotLib.h"
 
 #define DEMO_NAME DemoSensorSimulator
@@ -38,79 +38,81 @@
 //-------------
 // OVERVIEW
 //-------------
-/*      
+/*
 The SensorSimulator provides virtual sensor input. The simulator
 is based on a 'environment' definition of lines. A sensor reports
 the distance to the closed line, based on the robot pose.
 */
 
 //-------------
-// DECLARATIONS 
+// DECLARATIONS
 //-------------
- 
+
 //-------------
-// INSTANCES 
-//-------------                 
+// INSTANCES
+//-------------
 
 TSensorSimulator SimSharpL( 45, 120);  // look to front-left at 45 degrees, 120 mm from center
 TSensorSimulator SimSharpM(  0, 120);  // look straight, 120 mm from robot center
 TSensorSimulator SimSharpR(-45, 120);  // look to front-right at 45 degrees, 120mm from center
 
-//-----------------------------------------------------------------------------            
-// DefaultDemoSetup - 
-//-----------------------------------------------------------------------------            
-//-----------------------------------------------------------------------------            
+//-----------------------------------------------------------------------------
+// DefaultDemoSetup -
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 void DefaultDemoSetup()
-{       
-   printf("DemoSetup for SensorSimulator.\n");    
-   
+{
+   printf("DemoSetup for SensorSimulator.\n");
+
    // Setup the 'environment'
-   SimulatorTrack.Offset(-400, -400);  // Move track origin relative to robot 
+   SimulatorTrack.Offset(-400, -400);  // Move track origin relative to robot
                                        // position for NEW lines.
    SimulatorTrack.AddRoboRama();       // load the roborama track
-   
-   // Add 3 crosses of 70mm (similar to soda cans) 
+
+   // Add 3 crosses of 70mm (similar to soda cans)
    SimulatorTrack.AddCross(1200, 600, 70);
    SimulatorTrack.AddCross(1800, 600, 70);
    SimulatorTrack.AddCross(3000, 600, 70);
-   
+
    // Dump track lines
    SimulatorTrack.Dump();
-  
+
    // Dump track point at group 2 (can be used by RobotPresenter)
-//   SimulatorTrack.DumpPoints(2);      
+//   SimulatorTrack.DumpPoints(2);
 
    // Register simulated sensor tasks. The taks will update
-   // the variable 'Distance'. Note that it is good enough 
+   // the variable 'Distance'. Note that it is good enough
    // to call it (only) from MainTaks, but it can be called
-   // each ms too (as required for the sharp sensor).    
+   // each ms too (as required for the sharp sensor).
    MainTasks.Add(FP_FNAME(SimSharpL));
    MainTasks.Add(FP_FNAME(SimSharpM));
    MainTasks.Add(FP_FNAME(SimSharpR));
-  
-   // report sensor output when moving, through the presentation module
-   Presentation.Mode = 1;
-   Presentation.Add("g0", SimSharpL.Distance);
-   Presentation.Add("g1", SimSharpM.Distance);
-   Presentation.Add("g2", SimSharpR.Distance);
-   
+
+// The presentation module has been depricated.
+// One day, I'll check if this demo requires an alternative.
+//   // report sensor output when moving, through the presentation module
+//   Presentation.Mode = 1;
+//   Presentation.Add("g0", SimSharpL.Distance);
+//   Presentation.Add("g1", SimSharpM.Distance);
+//   Presentation.Add("g2", SimSharpR.Distance);
+
 }
 
-//----------------------------------------------------------------------------- 
-// CliCmd_DefaultDemo -           
-//-----------------------------------------------------------------------------            
-//-----------------------------------------------------------------------------            
+//-----------------------------------------------------------------------------
+// CliCmd_DefaultDemo -
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 void CliCmd_DefaultDemo(int NrParams, TCiParams *P)
-{  
-   printf("Demo command for SensorSimulator.\n"); 
+{
+   printf("Demo command for SensorSimulator.\n");
    printf("Use um commands to let the robot drive and see\n");
-   printf("the sensor data in RobotPresenter\n");    
-   
+   printf("the sensor data in RobotPresenter\n");
+
    printf("Current value of simulated SharpL: %d mm\n", SimSharpL.Distance);
    printf("Current value of simulated SharpM: %d mm\n", SimSharpM.Distance);
    printf("Current value of simulated SharpR: %d mm\n", SimSharpR.Distance);
-}   
+}
 
 //-------------
-// OTHER CODE 
+// OTHER CODE
 //-------------
--- Demo/TestPid.cpp
+++ Demo/TestPid.cpp
@@ -1,36 +1,36 @@
 //-----------------------------------------------------------------------------
 // TestPid.cpp
 //
-// Copyright (c) 2013-2018 Joep Suijs - All rights reserved.        
+// Copyright (c) 2013-2019 Joep Suijs - All rights reserved.
 //
 // Test of the PID controller.
 //
 // RobotLib tags: DEMO
 //-----------------------------------------------------------------------------
 // This file is part of RobotLib.
-// 
+//
 // RobotLib is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // RobotLib is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
-// along with RobotLib.  If not, see <http://www.gnu.org/licenses/>.
-// 
+// along with RobotLib. If not, see <http://www.gnu.org/licenses/>.
+//
 // See http://wiki.robotmc.be/index.php?title=RobotLib for more information.
 //-----------------------------------------------------------------------------
 // This is a DEMO file.
 // A DEMO file can be included in a project by adding it to makefile.do.
-// For details, see: 
+// For details, see:
 //    http://wiki.robotmc.be/index.php?title=RobotLib_Modules#RobotLib_Demo.27s
 //-----------------------------------------------------------------------------
 // tags_end
-//----------------------------------------------------------------------------- 
+//-----------------------------------------------------------------------------
 #include "RobotLib.h"
 
 #define DEMO_NAME TestPIDController
@@ -41,93 +41,95 @@
 //-------------
 // OVERVIEW
 //-------------
-/*                          
+/*
    A PID controller is control loop feedback mechanism for
-   (more or less) linear processes. 
+   (more or less) linear processes.
 
    The core of this PID controller is Brett Beauregard excellent
-   PID libarary for arduino.   
-   
+   PID libarary for arduino.
+
    This core routines are integrated into robotlib through the
    takt class and command line interface.
 */
 
 //-------------
-// DECLARATIONS 
+// DECLARATIONS
 //-------------
 void PidTestTakt();
-int  Simulator(int);   
- 
+int  Simulator(int);
+
 //-------------
-// INSTANCES 
+// INSTANCES
 //-------------
 TPID Pid1;
 
 static int MyInput, MySetpoint;
 
-//-----------------------------------------------------------------------------            
-// DefaultDemoSetup - 
-//-----------------------------------------------------------------------------            
-//-----------------------------------------------------------------------------            
+//-----------------------------------------------------------------------------
+// DefaultDemoSetup -
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 void DefaultDemoSetup()
-{       
-   printf("DemoSetup for PID controller.\n");  
-   
+{
+   printf("DemoSetup for PID controller.\n");
+
    // Setup PID controller
    Pid1.SetSampleTime(1);                    // Sample time in milliseconds.
    Pid1.SetTunings(2.0, 0.5, 2.0);           // Set Kp, Ki & Kd
    Pid1.Name      = "DemoPid";
    Pid1.Input.SetSrc(MyInput);               // Tell the PID where the input and setpoint vars are.
    Pid1.Setpoint.SetSrc(MySetpoint);
-   
-   Pid1.SetOutputLimits(-2550, 2550);    
+
+   Pid1.SetOutputLimits(-2550, 2550);
 #ifdef SIM_REVERSE
    Pid1.Reverse = true;
 #endif
    MsTasks.Add(FP_FNAME(Pid1));
 
    MainTasks.Add(FP_FNAME(PidTestTakt));
-   
+
    MySetpoint = 1800;
-   
+
    // temporary
-   CliAddCommands(CliPid, "Pid");    
-   
-   Presentation.Mode = 2;  // continues
-   Presentation.Add("g0", MySetpoint);
-   Presentation.Add("g1", MyInput);
-   Presentation.Add("g2", Pid1.Output);
-   
+   CliAddCommands(CliPid, "Pid");
+
+// The presentation module has been depricated.
+// One day, I'll check if this demo requires an alternative.
+   //Presentation.Mode = 2;  // continues
+   //Presentation.Add("g0", MySetpoint);
+   //Presentation.Add("g1", MyInput);
+   //Presentation.Add("g2", Pid1.Output);
+
 }
 
-//----------------------------------------------------------------------------- 
-// CliCmd_DefaultDemo -           
-//-----------------------------------------------------------------------------            
-//-----------------------------------------------------------------------------            
+//-----------------------------------------------------------------------------
+// CliCmd_DefaultDemo -
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 void CliCmd_DefaultDemo(int NrParams, TCiParams *P)
-{  
+{
    printf("Demo command for PID controller.\n");
 
    if (NrParams == 0) {
       // no params -> print output
-      printf("Pid %s %d -> %d\n", Pid1.Name, MyInput, Pid1.Output);  
+      printf("Pid %s %d -> %d\n", Pid1.Name, MyInput, Pid1.Output);
 
 
 
       return;
    }
-   if (NrParams == 1) {  
+   if (NrParams == 1) {
       printf("Setpoint: %d\n", P[0].PInt);
       MySetpoint = P[0].PInt;
    }
-}   
+}
 
 //-------------
-// OTHER CODE 
-//-------------                                  
+// OTHER CODE
+//-------------
 
 //-----------------------------------------------------------------------------
-// PidTestTakt -    
+// PidTestTakt -
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 void PidTestTakt()
@@ -135,10 +137,10 @@ void PidTestTakt()
 
    if (PidAutoTuner.IsDone()) {
       //we're done, set the tuning parameters
-      PidAutoTuner.Preview();   
-      PidAutoTuner.Commit();    
+      PidAutoTuner.Preview();
+      PidAutoTuner.Commit();
    }
-    
+
    //send-receive with processing if it's time
    static unsigned long serialTime;
    if(millis()>serialTime) {
@@ -150,21 +152,21 @@ void PidTestTakt()
       } else {
          printf("tuning mode\n");
       }
-   } 
-   
-   
+   }
+
+
    // feed output via simulator to input
-   MyInput = Simulator(Pid1.Output);   
+   MyInput = Simulator(Pid1.Output);
 
 
    if (Interval-- < 0) {
       Interval = 10;
       //printf("Pid %s %d -> %d\n", Pid1.Name, MyInput, Pid1.Output);
    }
-}             
+}
 
 
-#include <stdlib.h>    
+#include <stdlib.h>
 // Autotune results Ku: 3.239798, Pu: 5.200000
 
 //-----------------------------------------------------------------------------
@@ -174,25 +176,25 @@ void PidTestTakt()
 int Simulator(int SimInput)
 {  static bool First = true;
    static int PrevSimOutput;
-   
+
    const float KpModel=1.5, Tau_p=100, outputStart=5;
    const int DelayLineLenght = 29;
    static int DelayLine[DelayLineLenght+1];
 
    if (First) {
       First = false;
-      // simulator setup 
+      // simulator setup
       for(int i=0;i<DelayLineLenght+1;i++) {
         DelayLine[i]=outputStart;
       }
    }
-   
+
 #ifdef SIM_REVERSE
    DelayLine[DelayLineLenght] = -SimInput;
 #else
    DelayLine[DelayLineLenght] = SimInput;
 #endif
-  
+
    //cycle the dead time
    for(int i=0;i<DelayLineLenght;i++) {
       DelayLine[i] = DelayLine[i+1];
@@ -200,7 +202,7 @@ int Simulator(int SimInput)
 
    //compute output of process -> input for PID
    int SimOutput = (KpModel / Tau_p) *(DelayLine[0]-outputStart) + PrevSimOutput*(1-1/Tau_p) + (rand() % 200) / 100.0 - 1;
-   PrevSimOutput = SimOutput; 
+   PrevSimOutput = SimOutput;
    //printf("Sim %d %d\n", SimInput, SimOutput);
    return SimOutput;
 
